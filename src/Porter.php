79a1c2b422303735e704996534247ac9d1973a7a
G Brabyn <gregorbrabyn@yahoo.co.uk>
2021-10-18 11:45:58 +1300

    Upgrading to PHP8

--- src/Porter.php
+++ src/Porter.php
@@ -84,35 +84,32 @@ class Porter
     {
         // Part a
         if (substr($word, -1) == 's') {
-
-               self::replace($word, 'sses', 'ss')
-            OR self::replace($word, 'ies', 'i')
-            OR self::replace($word, 'ss', 'ss')
-            OR self::replace($word, 's', '');
+            self::replace($word, 'sses', 'ss')
+            or self::replace($word, 'ies', 'i')
+            or self::replace($word, 'ss', 'ss')
+            or self::replace($word, 's', '');
         }
 
         // Part b
-        if (substr($word, -2, 1) != 'e' OR !self::replace($word, 'eed', 'ee', 0)) { // First rule
+        if (substr($word, -2, 1) != 'e' or !self::replace($word, 'eed', 'ee', 0)) { // First rule
             $v = self::$regex_vowel;
 
             // ing and ed
-            if (   preg_match("#$v+#", substr($word, 0, -3)) && self::replace($word, 'ing', '')
-                OR preg_match("#$v+#", substr($word, 0, -2)) && self::replace($word, 'ed', '')) { // Note use of && and OR, for precedence reasons
+            if (preg_match("#$v+#", substr($word, 0, -3)) && self::replace($word, 'ing', '')
+                or preg_match("#$v+#", substr($word, 0, -2)) && self::replace($word, 'ed', '')) { // Note use of && and OR, for precedence reasons
 
                 // If one of above two test successful
-                if (    !self::replace($word, 'at', 'ate')
-                    AND !self::replace($word, 'bl', 'ble')
-                    AND !self::replace($word, 'iz', 'ize')) {
+                if (!self::replace($word, 'at', 'ate')
+                    and !self::replace($word, 'bl', 'ble')
+                    and !self::replace($word, 'iz', 'ize')) {
 
                     // Double consonant ending
-                    if (    self::doubleConsonant($word)
-                        AND substr($word, -2) != 'll'
-                        AND substr($word, -2) != 'ss'
-                        AND substr($word, -2) != 'zz') {
-
+                    if (self::doubleConsonant($word)
+                        and substr($word, -2) != 'll'
+                        and substr($word, -2) != 'ss'
+                        and substr($word, -2) != 'zz') {
                         $word = substr($word, 0, -1);
-
-                    } elseif (self::m($word) == 1 AND self::cvc($word)) {
+                    } elseif (self::m($word) == 1 and self::cvc($word)) {
                         $word .= 'e';
                     }
                 }
@@ -148,12 +145,12 @@ class Porter
         switch (substr($word, -2, 1)) {
             case 'a':
                    self::replace($word, 'ational', 'ate', 0)
-                OR self::replace($word, 'tional', 'tion', 0);
+                or self::replace($word, 'tional', 'tion', 0);
                 break;
 
             case 'c':
                    self::replace($word, 'enci', 'ence', 0)
-                OR self::replace($word, 'anci', 'ance', 0);
+                or self::replace($word, 'anci', 'ance', 0);
                 break;
 
             case 'e':
@@ -166,29 +163,29 @@ class Porter
 
             case 'l':
                    self::replace($word, 'entli', 'ent', 0)
-                OR self::replace($word, 'ousli', 'ous', 0)
-                OR self::replace($word, 'alli', 'al', 0)
-                OR self::replace($word, 'bli', 'ble', 0)
-                OR self::replace($word, 'eli', 'e', 0);
+                or self::replace($word, 'ousli', 'ous', 0)
+                or self::replace($word, 'alli', 'al', 0)
+                or self::replace($word, 'bli', 'ble', 0)
+                or self::replace($word, 'eli', 'e', 0);
                 break;
 
             case 'o':
                    self::replace($word, 'ization', 'ize', 0)
-                OR self::replace($word, 'ation', 'ate', 0)
-                OR self::replace($word, 'ator', 'ate', 0);
+                or self::replace($word, 'ation', 'ate', 0)
+                or self::replace($word, 'ator', 'ate', 0);
                 break;
 
             case 's':
                    self::replace($word, 'iveness', 'ive', 0)
-                OR self::replace($word, 'fulness', 'ful', 0)
-                OR self::replace($word, 'ousness', 'ous', 0)
-                OR self::replace($word, 'alism', 'al', 0);
+                or self::replace($word, 'fulness', 'ful', 0)
+                or self::replace($word, 'ousness', 'ous', 0)
+                or self::replace($word, 'alism', 'al', 0);
                 break;
 
             case 't':
                    self::replace($word, 'biliti', 'ble', 0)
-                OR self::replace($word, 'aliti', 'al', 0)
-                OR self::replace($word, 'iviti', 'ive', 0);
+                or self::replace($word, 'aliti', 'al', 0)
+                or self::replace($word, 'iviti', 'ive', 0);
                 break;
         }
 
@@ -213,7 +210,7 @@ class Porter
 
             case 't':
                    self::replace($word, 'icate', 'ic', 0)
-                OR self::replace($word, 'iciti', 'ic', 0);
+                or self::replace($word, 'iciti', 'ic', 0);
                 break;
 
             case 'u':
@@ -246,7 +243,7 @@ class Porter
 
             case 'c':
                    self::replace($word, 'ance', '', 1)
-                OR self::replace($word, 'ence', '', 1);
+                or self::replace($word, 'ence', '', 1);
                 break;
 
             case 'e':
@@ -259,19 +256,19 @@ class Porter
 
             case 'l':
                    self::replace($word, 'able', '', 1)
-                OR self::replace($word, 'ible', '', 1);
+                or self::replace($word, 'ible', '', 1);
                 break;
 
             case 'n':
                    self::replace($word, 'ant', '', 1)
-                OR self::replace($word, 'ement', '', 1)
-                OR self::replace($word, 'ment', '', 1)
-                OR self::replace($word, 'ent', '', 1);
+                or self::replace($word, 'ement', '', 1)
+                or self::replace($word, 'ment', '', 1)
+                or self::replace($word, 'ent', '', 1);
                 break;
 
             case 'o':
-                if (substr($word, -4) == 'tion' OR substr($word, -4) == 'sion') {
-                   self::replace($word, 'ion', '', 1);
+                if (substr($word, -4) == 'tion' or substr($word, -4) == 'sion') {
+                    self::replace($word, 'ion', '', 1);
                 } else {
                     self::replace($word, 'ou', '', 1);
                 }
@@ -283,7 +280,7 @@ class Porter
 
             case 't':
                    self::replace($word, 'ate', '', 1)
-                OR self::replace($word, 'iti', '', 1);
+                or self::replace($word, 'iti', '', 1);
                 break;
 
             case 'u':
@@ -313,9 +310,7 @@ class Porter
         if (substr($word, -1) == 'e') {
             if (self::m(substr($word, 0, -1)) > 1) {
                 self::replace($word, 'e', '');
-
             } elseif (self::m(substr($word, 0, -1)) == 1) {
-
                 if (!self::cvc(substr($word, 0, -1))) {
                     self::replace($word, 'e', '');
                 }
@@ -323,7 +318,7 @@ class Porter
         }
 
         // Part b
-        if (self::m($word) > 1 AND self::doubleConsonant($word) AND substr($word, -1) == 'l') {
+        if (self::m($word) > 1 and self::doubleConsonant($word) and substr($word, -1) == 'l') {
             $word = substr($word, 0, -1);
         }
 
@@ -351,7 +346,7 @@ class Porter
 
         if (substr($str, $len) == $check) {
             $substr = substr($str, 0, $len);
-            if (is_null($m) OR self::m($substr) > $m) {
+            if (is_null($m) or self::m($substr) > $m) {
                 $str = $substr . $repl;
             }
 
@@ -402,7 +397,7 @@ class Porter
     {
         $c = self::$regex_consonant;
 
-        return preg_match("#$c{2}$#", $str, $matches) AND $matches[0]{0} == $matches[0]{1};
+        return preg_match("#$c{2}$#", $str, $matches) and substr($matches[0], 0, 1) == substr($matches[0], 1, 1);
     }
 
     /**
@@ -417,10 +412,14 @@ class Porter
         $c = self::$regex_consonant;
         $v = self::$regex_vowel;
 
-        return     preg_match("#($c$v$c)$#", $str, $matches)
-               AND strlen($matches[1]) == 3
-               AND $matches[1]{2} != 'w'
-               AND $matches[1]{2} != 'x'
-               AND $matches[1]{2} != 'y';
+        return preg_match("#($c$v$c)$#", $str, $matches) &&
+            strlen($matches[1]) == 3 &&
+            !in_array(substr($matches[1], 2, 1), ['w', 'x', 'y']);
+
+        // return     preg_match("#($c$v$c)$#", $str, $matches)
+        //        AND strlen($matches[1]) == 3
+        //        AND $matches[1]{2} != 'w'
+        //        AND $matches[1]{2} != 'x'
+        //        AND $matches[1]{2} != 'y';
     }
 }
